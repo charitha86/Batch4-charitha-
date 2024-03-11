56f4d095d345030c35a344f27147aa71e6b409af
charitha86 <162983741+charitha86@users.noreply.github.com>
2024-03-11 16:22:53 +0530

    Add files via upload

---
+++ charitha reddy.py
@@ -0,0 +1,36 @@
+"""
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+"""
+"""
+1.} commenting line is used for making documentation in python file
+2.} when the line is commented,that line will not be considered as a part of the code
+3.}when the program is excecuted, commented line will not be in the part of the excecution
+-------->single line commenting can be done using# at the beginning of the line
+ eg: #print("hello world")
+----> multiple line commenting  can be done using"""  """ the lines b/w quoytes will be commented
+"""
+eg:"""
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+"""
+#print("hello world")
+import this
+import antigravity
+
+
+
